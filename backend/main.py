a4c2fba7dfcc8587d7ce1323bb94a0551f6e8833
Shifa20 <shifa20naqvi@gmail.com>
2026-05-12 21:57:45 +0530

    Updated CORS settings for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://vectorshift-flow-builder.vercel.app"
+
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
